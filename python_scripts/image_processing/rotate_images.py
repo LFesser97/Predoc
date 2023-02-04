891e7587fd1648f6b8be46ff17e25ffb81cf5356
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-02-04 09:53:55 -0500

    debugged cropping

--- python_scripts/image_processing/rotate_images.py
+++ python_scripts/image_processing/rotate_images.py
@@ -19,17 +19,63 @@ import cv2
 
 def rotate_image(image, angle):
     """
-    Rotates an image (angle in degrees) and expands image to avoid cropping
+    Rotates an image by a given angle.
+    Resizes the image to avoid cropping.
+
+    Parameters
+    ----------
+    image : numpy.ndarray
+        Image to rotate.
+
+    angle : int
+        Angle in degrees to rotate image by.
+
+    Returns
+    -------
+    rotated_image : numpy.ndarray
+        Rotated image.
     """
-    image_center = tuple(np.array(image.shape[1::-1]) / 2)
-    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
-    return result
+    # Get image dimensions
+    height, width = image.shape[:2]
+    
+    # Get rotation matrix
+    rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1)
+    
+    # Calculate new image dimensions
+    abs_cos = abs(rotation_matrix[0, 0])
+    abs_sin = abs(rotation_matrix[0, 1])
+    new_width = int(height * abs_sin + width * abs_cos)
+    new_height = int(height * abs_cos + width * abs_sin)
+    
+    # Adjust rotation matrix
+    rotation_matrix[0, 2] += (new_width - width) / 2
+    rotation_matrix[1, 2] += (new_height - height) / 2
+    
+    # Rotate image
+    rotated_image = cv2.warpAffine(image, rotation_matrix, (new_width, new_height))
+    
+    return rotated_image
+
 
 
 def rotate_images(input_folder, output_folder, k):
     """
     Rotates all images in a given input folder by k times 90 degrees, where k is an input integer. 
+
+    Parameters
+    ----------
+    input_folder : str
+        Path to input folder.
+
+    output_folder : str
+        Path to output folder.
+    
+    k : int
+        Number of times to rotate image by 90 degrees (to the left)
+
+    Returns
+    -------
+    None.
     """
     # Create output folder if it does not exist
     if not os.path.exists(output_folder):
@@ -57,6 +103,6 @@ def main(input_folder, output_folder, k):
 input_folder = '' # Path to input folder
 output_folder = '' # Path to output folder
 
-k = 1 # Number of times to rotate image by 90 degrees
+k = -1 # Number of times to rotate image by 90 degrees
 
 main(input_folder, output_folder, k)
