8d18b962402313f160c8e206885007d1c451cff1
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-03-31 19:05:47 -0400

    added DAL and h-divergence

---
+++ topic_and_content_classification/discriminative_active_learning.py
@@ -0,0 +1,48 @@
+"""
+discriminative_active_learning.py
+
+Created on Fri Mar 31 2023
+
+@author: Lukas
+
+This file contains all methods to perform discriminative active learning.
+"""
+
+# import packages
+
+# import functions form other files in this project
+
+# functions for measuring the quality of a batch
+
+def get_diversity(dataset):
+    """
+    Compute the diversity of a dataset.
+
+    Parameters
+    ----------
+    dataset : torch.utils.data.Dataset
+        The dataset to compute the diversity for.
+
+    Returns
+    -------
+    diversity : float
+        The diversity of the dataset.
+    """
+    pass
+
+
+def get_representativeness(dataset):
+    """
+    Compute the representativeness of a dataset.
+
+    Parameters
+    ----------
+    dataset : torch.utils.data.Dataset
+        The dataset to compute the representativeness for.
+
+    Returns
+    -------
+    representativeness : float
+        The representativeness of the dataset.
+    """
+    pass
