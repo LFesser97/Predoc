9c49cba9ce1ce5c07c782fbea401004ebc4a7e0f
LFesser97 <53827774+LFesser97@users.noreply.github.com>
2023-06-19 16:43:18 +0200

    added custom datasets for toy examples

---
+++ exact_subgraph_matching/customizations/custom_csvs.py
@@ -0,0 +1,13 @@
+"""
+custom_csvs.py
+
+Created on Mon Jun 19 2023
+
+@author: Lukas
+
+This files contains functions for creating custom csvs from pandas dataframes.
+"""
+
+import numpy as np
+import pandas as pd
+
